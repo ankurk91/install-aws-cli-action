29ba99c7c1968fcb9b5dea433e42787b4bb71943
ankurk91 <pro.ankurk1@gmail.com>
2025-05-25 20:01:34 +0530

    refactor: POSIX-compliant script

--- scripts/verify-aws-cli.sh
+++ scripts/verify-aws-cli.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if command -v aws &>/dev/null; then
+if command -v aws >/dev/null 2>&1; then
     echo "AWS_CLI_PREINSTALLED=true" >> "$GITHUB_OUTPUT"
     echo "AWS CLI is already installed."
     aws --version
