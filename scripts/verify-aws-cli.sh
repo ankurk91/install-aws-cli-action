80582c889fec24274cd35e735e01562ee01996eb
ankurk91 <pro.ankurk1@gmail.com>
2025-05-25 20:09:29 +0530

    better message for user

--- scripts/verify-aws-cli.sh
+++ scripts/verify-aws-cli.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if command -v aws >/dev/null 2>&1; then
+    echo "AWS CLI is already installed. Skipping install."
     echo "AWS_CLI_PREINSTALLED=true" >> "$GITHUB_OUTPUT"
-    echo "AWS CLI is already installed."
     aws --version
     exit 0
 fi
